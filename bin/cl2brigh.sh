4ceeb79876d32f788cfd9596ec3a1f3537ebe6f0
Ashwin Aji <ashwinma@gmail.com>
2015-07-16 03:40:54 -0500

    version changed to 0.1.1
    Former-commit-id: 09eba7ba5ee3df3ed4839d9c8fcef656fc95c055

--- bin/cl2brigh.sh
+++ bin/cl2brigh.sh
@@ -23,7 +23,7 @@
 #  Written by Greg Rodgers  Gregory.Rodgers@amd.com
 #  Maintained by Shreyas Ramalingam Shreyas.Ramalingam@amd.com
 #
-PROGVERSION=0.1.0
+PROGVERSION=0.1.1
 #
 # Copyright (c) 2015 ADVANCED MICRO DEVICES, INC.  Patent pending.
 # 

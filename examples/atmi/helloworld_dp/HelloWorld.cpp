e2b8b409e4ede05af1947da5ac1a5fc398730d52
Jing Zhang <Jing.Zhang2@amd.com>
2015-07-29 16:51:05 -0400

    clean codes of helloworld_dp
    Former-commit-id: 2ba13b31b51e39dfeb9c0055f74f0e7e121cba75

--- examples/atmi/helloworld_dp/HelloWorld.cpp
+++ examples/atmi/helloworld_dp/HelloWorld.cpp
@@ -45,41 +45,13 @@ int main(int argc, char* argv[]) {
 	char *output_gpu = (char*) malloc((strlength + 1) * numTasks);
 
     ATMI_LPARM_1D(lparm, numTasks);
-    lparm->synchronous = ATMI_FALSE;
+    lparm->synchronous = ATMI_TRUE;
 
     lparm->kernel_id = 0;
     decode_kl_init(lparm);
     print_kl_init(lparm);
     atmi_task_t *task = split(lparm, input, output_gpu, strlength);
 
-    while(hsa_signal_load_relaxed(*((hsa_signal_t *)task->handle)) > 0)
-    {
-        //for(int i = 0; i < numTasks; i++)
-            //printf("%d-%d ", i, output_gpu[i * (strlength + 1)]);
-        //printf("\n");
-        //printf("gpu_kernarg_offset: %d\n", atmi_klist[1].gpu_kernarg_offset);
-        //printf("cpu_kernarg_offset: %d\n", atmi_klist[2].cpu_kernarg_offset);
-    }
-
-
-    //for(int i = 0; i < numTasks; i++)
-    //{
-    //for(int j = 0; j < strlength; j++)
-    //printf("%c", output_gpu[i * (strlength + 1) + j]);
-    //printf("\n");
-        //output_gpu[strlength * (i + 1)] = '\0';
-        //cout << output_gpu + i * strlength << endl;
-        //}
-
-    //decode(lparm, input, output_gpu, strlength);
-    // 
-    //lparm->kernel_id = K_ID_decode_cpu;
-    //decode(lparm, input, output_cpu, strlength);
-    //output_cpu[strlength] = '\0';
-
-    //cout << "Output from the CPU: " << output_cpu << endl;
-    //cout << "Output from the GPU: " << output_gpu << endl;
-    //free(output_cpu);
 	free(output_gpu);
 	return 0;
 }

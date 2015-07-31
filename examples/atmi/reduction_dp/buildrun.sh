cfed7a05f91454475722df039ce523a60cf2782f
Jing <Jing.Zhang2@amd.com>
2015-07-31 16:35:57 -0500

    change hw.cl in reduction_dp to reduction.cl
    Former-commit-id: cb21dbf3a30b067a4592c1dbcbdc172205dd026a

--- examples/atmi/reduction_dp/buildrun.sh
+++ examples/atmi/reduction_dp/buildrun.sh
@@ -16,8 +16,8 @@ export LD_LIBRARY_PATH=$HSA_RUNTIME_PATH/lib:$ATMI_RUNTIME_PATH/lib:$LD_LIBRARY_
 # in Reduction.cpp.pifdefs.c
 #echo 
 if [ -f hello ] ; then rm hello ; fi
-echo g++ -c -o Reduction.o Reduction.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=hw.cl -O3 -I$ATMI_INC
-g++-4.9 -c -o Reduction.o Reduction.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=hw.cl -fplugin-arg-atmi_pifgen-pifgenfile=Reduction.cpp.pifdefs.c -O3 -I$ATMI_INC
+echo g++ -c -o Reduction.o Reduction.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=reduction.cl -O3 -I$ATMI_INC
+g++-4.9 -c -o Reduction.o Reduction.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=reduction.cl -fplugin-arg-atmi_pifgen-pifgenfile=Reduction.cpp.pifdefs.c -O3 -I$ATMI_INC
 
 echo g++ -o hello Reduction.o Reduction.cpp.pifdefs.c -g -O3 -lelf -L$ATMI_RUNTIME_PATH/lib -latmi_runtime -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 -I$ATMI_INC -I$HSA_RUNTIME_PATH/include
 g++-4.9 -o reduction Reduction.o Reduction.cpp.pifdefs.c -g -O3 -lelf -L$ATMI_RUNTIME_PATH/lib -latmi_runtime -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 -I$ATMI_INC -I$HSA_RUNTIME_PATH/include

62deb29af83fd1b42b7e719dd1f483048a925f2d
Ashwin Aji <ashwinma@gmail.com>
2016-06-01 18:27:17 -0500

    minor cleanup
    Former-commit-id: 10b3fce2d597faf5066ca120918c5af7aaeb16cd

--- examples/eps/buildrun.sh
+++ examples/eps/buildrun.sh
@@ -14,8 +14,8 @@ echo
 if [ -f eps ] ; then rm eps ; fi
 echo g++ -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -fplugin-arg-atmi_pifgen-jitcompile=false -O3 -I$ATMI_INC
 #vtc++ -vt:inst compinst -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -O3 -I$ATMI_INC
-g++ -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -fplugin-arg-atmi_pifgen-jitcompile=false -O3 -I$ATMI_INC
-#g++ -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -O3 -I$ATMI_INC
+#g++ -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -fplugin-arg-atmi_pifgen-jitcompile=false -O3 -I$ATMI_INC
+g++ -c -o nullKernel.o eps.cpp -g -fplugin=atmi_pifgen.so -fplugin-arg-atmi_pifgen-clfile=nullKernel.cl -O3 -I$ATMI_INC
 
 echo g++ -o eps nullKernel.o eps.cpp.pifdefs.c -g -O3 -lelf -L$ATMI_PATH/lib -latmi_runtime -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 -I$ATMI_INC -I$HSA_RUNTIME_PATH/include
 #vtc++ -vt:inst compinst -o eps nullKernel.o eps.cpp.pifdefs.c -g -O3 -lelf -L$ATMI_PATH/lib -latmi_runtime -L$HSA_RUNTIME_PATH/lib -lhsa-runtime64 -I$ATMI_INC -I$HSA_RUNTIME_PATH/include

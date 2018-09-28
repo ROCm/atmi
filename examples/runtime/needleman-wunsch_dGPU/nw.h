816aa1c73b987acb7f22ca71ed5c0286f0b6bfac
Ashwin Aji <ashwinma@gmail.com>
2018-09-28 21:12:48 +0000

    adding needleman-wunsch example for discrete GPUs
    Former-commit-id: ae0658565c44046032c80fe23e864d509c9a7a84

---
+++ examples/runtime/needleman-wunsch_dGPU/nw.h
@@ -0,0 +1,10 @@
+/*===--------------------------------------------------------------------------
+ *              ATMI (Asynchronous Task and Memory Interface)
+ *
+ * This file is distributed under the MIT License. See LICENSE.txt for details.
+ *===------------------------------------------------------------------------*/
+
+#define BLOCK_SIZE 32
+#define LIMIT -999
+
+

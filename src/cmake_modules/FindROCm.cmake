ROCm/atmi

commit 6b7fc7e1d7891197d048a140ae0f6470db55918c

Increased the range of include directories to look into for ROCr. (#80)

src/cmake_modules/FindROCm.cmake
@@ -19,11 +19,11 @@ find_path(
     hsa.h
   HINTS
     ${ROC_DIR}/include
-    ${ROCR_DIR}/include
-    ${ROCR_DIR}/include/hsa
-    ${ROCR_DIR}/hsa/include
-    ${ROCR_DIR}/hsa/include/hsa
-    ${ROCR_DIR}
+    ${ROC_DIR}/include/hsa
+    ${ROC_DIR}/hsa/include
+    ${ROC_DIR}/hsa/include/hsa
+    ${ROC_DIR}/hsa
+    ${ROC_DIR}
     /opt/rocm/include
     /opt/rocm/hsa/include
     /usr/local/include
@@ -37,8 +37,7 @@ find_library(
     hsa-runtime64
   HINTS
     ${ROC_DIR}/lib
-    ${ROCR_DIR}/lib
-    ${ROCR_DIR}
+    ${ROC_DIR}
     /opt/rocm/lib
     /opt/rocm/hsa/lib
     /usr/local/lib
@@ -52,8 +51,7 @@ find_library(
     hsakmt
   HINTS
     ${ROC_DIR}/lib
-    ${ROCT_DIR}/lib
-    ${ROCT_DIR}
+    ${ROC_DIR}
     /opt/rocm/lib
     /opt/rocm/hsa/lib
     /usr/local/lib

commit 73e13b2f0ed6ffaa6734e35b612e02db017bd528

Increased the range of include directories to look into for ROCr some more (#81)
* One can specify the ROCR and ROCT library paths individually if need be.
* The patch 9ac42a3 explicitly added these options. The patch 6b7fc7e inadvertently removed them. Adding them back.

(#80)

src/cmake_modules/FindROCm.cmake
@@ -24,6 +24,11 @@ find_path(
     ${ROC_DIR}/hsa/include/hsa
     ${ROC_DIR}/hsa
     ${ROC_DIR}
+    ${ROCR_DIR}/include
+    ${ROCR_DIR}/include/hsa
+    ${ROCR_DIR}/hsa/include
+    ${ROCR_DIR}/hsa/include/hsa
+    ${ROCR_DIR}
     /opt/rocm/include
     /opt/rocm/hsa/include
     /usr/local/include
@@ -38,6 +43,8 @@ find_library(
   HINTS
     ${ROC_DIR}/lib
     ${ROC_DIR}
+    ${ROCR_DIR}/lib
+    ${ROCR_DIR}
     /opt/rocm/lib
     /opt/rocm/hsa/lib
     /usr/local/lib
@@ -52,6 +59,8 @@ find_library(
   HINTS
     ${ROC_DIR}/lib
     ${ROC_DIR}
+    ${ROCT_DIR}/lib
+    ${ROCT_DIR}
     /opt/rocm/lib
     /opt/rocm/hsa/lib
     /usr/local/lib